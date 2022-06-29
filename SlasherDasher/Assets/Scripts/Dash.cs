8d72dde2bd76bef038a872034927fe30eadb564e
Ch336ic0 <108430950+Ch336ic0@users.noreply.github.com>
2022-06-29 18:41:21 -0300

    Made DashDirection script

--- SlasherDasher/Assets/Scripts/Dash.cs
+++ SlasherDasher/Assets/Scripts/Dash.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class Dash : MonoBehaviour
 {
     public GameObject DashArrow;
-    void Start()
+
+    private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            while (Input.GetMouseButtonDown(0))
+            {
+                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+                mousePos.Normalize();
+
+                float rotationZ = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+                transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+            }
+
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }

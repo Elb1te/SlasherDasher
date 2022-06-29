6ab873b01f8a881c90e895947a2a5170571a5b51
Ch336ic0 <108430950+Ch336ic0@users.noreply.github.com>
2022-06-29 19:05:03 -0300

    Dash script updated

--- SlasherDasher/Assets/Scripts/Dash.cs
+++ SlasherDasher/Assets/Scripts/Dash.cs
@@ -5,20 +5,32 @@ using UnityEngine;
 public class Dash : MonoBehaviour
 {
     public GameObject DashArrow;
+    
 
     private void Update()
     {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        mousePos.Normalize();
+
+        float rotationZ = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        DashArrow.transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+
         if (Input.GetMouseButtonDown(0))
         {
-                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-                mousePos.Normalize();
-
-                float rotationZ = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-
-                transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
-
+            DoDash();
         }
 
     }
 
+    private void DoDash()
+    {
+        Debug.Log("Dashed!");
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        mousePos.Normalize();
+
+        float rotationZ = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+    }
 }
